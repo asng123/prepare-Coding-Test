27081df39932bae3fce9b03cf5c33c5e41d6f75f
asng123 <80505960+asng123@users.noreply.github.com>
2023-01-05 16:27:41 +0900

    [level 1] Title: K번째수, Time: 0.23 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] array, int[][] commands) {
+        // 답
+        ArrayList<Integer> answer = new ArrayList<>();
+        // 자른배열
+        ArrayList<Integer> temp = new ArrayList<>();
+        for(int i = 0; i< commands.length;i++){
+            // 자른배열 만들기
+            for(int j = commands[i][0]-1; j< commands[i][1];j++){
+                temp.add(array[j]);
+            }
+            // 정렬
+            // ** ArrayList 정렬 방법
+            // temp.sort(Comparator.naturalOrder());
+            Collections.sort(temp);
+            
+            answer.add(temp.get(commands[i][2]-1));
+            temp.clear();
+        }
+        
+        return answer;
+    }
+}

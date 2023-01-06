7631f86682f31a7ea87a31e6c8e59b9482290059
asng123 <80505960+asng123@users.noreply.github.com>
2023-01-06 09:53:01 +0900

    Update 폰켓몬.java

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,20 +1,18 @@
 import java.util.*;
 import java.io.*;
 
+// HashSet
+// add, addAll, contains, containsAll, isEmpty, remove, removeAll, size, toArray
 class Solution {
     public int solution(int[] nums) {
-        HashSet<Integer> map = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>(); // HashSet 선언
         
-        //set은 add map은 put
-        for(int i = 0; i< nums.length; i++){
-            map.add(nums[i]);
-        }
+        for(int i = 0; i< nums.length; i++) // nums를 순회하며
+            set.add(nums[i]);               // 중복제거를 위해 set에 넣어준다
         
-        //map의 사이즈가 폰켓몬수의 절반보다 크다면, 중복되지 않게 전부 챙길 수 있고
-        if(map.size() >= nums.length/2)
+        if(set.size() >= nums.length/2) // 중복제거된 것이 절반보다 클때 최대값 가지고
             return nums.length/2;
-        //그게 아니라면 최대 map.size만큼
-        else
-           return map.size();        
+        else                            // 그게 아니라면 최대 set.size()만큼 가진다
+           return set.size();        
     }
 }

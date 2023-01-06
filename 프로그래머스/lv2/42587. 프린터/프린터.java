e6271c70743a8ef326ee5161653fb58599b1ac50
asng123 <80505960+asng123@users.noreply.github.com>
2023-01-06 10:50:56 +0900

    [level 2] Title: 프린터, Time: 0.42 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.java
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -4,35 +4,25 @@ import java.io.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        // 우선순위 배열 복사
-        int[] print = priorities.clone();
-        // 우선순위 배열 정렬
-        Arrays.sort(priorities);
+
+        int[] print = priorities.clone();   // 우선순위 배열 복사
+        Arrays.sort(priorities);            // 우선순위 배열 정렬
         
-        //dq에 우선순위와 index를 함께 넣는다.
         Deque<int[]> dq = new ArrayDeque<>();
-        for(int i = 0; i<print.length; i++)
-            dq.addFirst(new int[] {print[i], i});
+        for(int i=0;i<print.length;i++)             // 정렬하지 않은 우선순위 배열을 순회하며
+            dq.addFirst(new int[] {print[i], i});   //dq에 우선순위와 index를 함께 넣는다.
                 
-        // 우선순위가 높은 것 먼저 나와야 해서 정렬 배열 뒤부터 확인.
-        int maxidx = print.length-1;
+        int maxidx = print.length-1;            // 우선순위가 높은것부터 보기 위해 배열 뒤부터 확인
         while(true){
-            // 프린터에서 꺼내보고
-            int[] cur = dq.pollLast();
-            //꺼낸 프린터의 우선순위 값이 현재 가장 높은 우선순위 값과 같은지 확인.
-            if(cur[0] == priorities[maxidx]){
-                //같으면 일단 꺼내고 몇번째로 나왔는지 체크하기위해 answer++
-                maxidx--;
-                answer++;                
-                //만약 내가 원하는 location의 문서였다면 break
-                if(cur[1] == location)
-                    break;   
+            int[] cur = dq.pollLast();          // 프린터에서 꺼내보고
+            if(cur[0] == priorities[maxidx]){   // 프린터에서 꺼낸 우선순위 값이 제일 높다면
+                maxidx--;                       // 꺼내고
+                answer++;                       // 몇번째로 나왔는지 체크하기 위해 answer++
+                if(cur[1] == location)          // 만약 내가 원한 위치의 인쇄물이었다면
+                    return answer;              // return answer     
             }
-            // 우선순위가 낮은 프린터라면 뒤로 넣어주기. 
-            else
-                dq.addFirst(cur);
+            else                                // 우선순위가 낮은 프린터라면
+                dq.addFirst(cur);               // 맨뒤로 다시 넣어주기
         }
-        
-        return answer;
     }
 }

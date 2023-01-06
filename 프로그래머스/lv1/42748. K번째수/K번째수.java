939f339f1be9ddbc5460362ac6d4f2a49a846e1b
asng123 <80505960+asng123@users.noreply.github.com>
2023-01-06 10:03:18 +0900

    Update K번째수.java

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,23 +1,19 @@
 import java.util.*;
 
+// ArrayList 정렬 방법
+// list.sort(Comparator.naturalOrder());
 class Solution {
     public ArrayList<Integer> solution(int[] array, int[][] commands) {
-        // 답
-        ArrayList<Integer> answer = new ArrayList<>();
-        // 자른배열
-        ArrayList<Integer> temp = new ArrayList<>();
-        for(int i = 0; i< commands.length;i++){
-            // 자른배열 만들기
-            for(int j = commands[i][0]-1; j< commands[i][1];j++){
+        ArrayList<Integer> answer = new ArrayList<>();  // return할 답
+
+        ArrayList<Integer> temp = new ArrayList<>();    // commands에 맞게 자른 배열
+        for(int i=0;i<commands.length;i++){                     // commands 순회하면서            
+            for(int j=commands[i][0]-1;j< commands[i][1];j++)  // 자른배열 만들기
                 temp.add(array[j]);
-            }
-            // 정렬
-            // ** ArrayList 정렬 방법
-            // temp.sort(Comparator.naturalOrder());
-            Collections.sort(temp);
-            //답안 넣기
-            answer.add(temp.get(commands[i][2]-1));
-            temp.clear();
+
+            Collections.sort(temp);                 // 자른배열 정렬
+            answer.add(temp.get(commands[i][2]-1)); // commands에 맞게 answer에 넣기
+            temp.clear();                           // temp 재사용을 위해 clear()
         }
         
         return answer;

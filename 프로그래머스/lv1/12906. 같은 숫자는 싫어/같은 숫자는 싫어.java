150b4df2a50c5586615c471a8370fac18f4d7e46
asng123 <80505960+asng123@users.noreply.github.com>
2023-01-06 09:45:06 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.44 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,19 +3,17 @@ import java.io.*;
 
 public class Solution {
     public ArrayList solution(int []arr) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        Deque<Integer> dq = new ArrayDeque<>();
+        ArrayList<Integer> answer = new ArrayList<>();  // return할 arraylist
         
-        //dq에 첫번째 원소 넣고
-        dq.add(arr[0]);
-        for(int i = 1; i < arr.length; i++){
-            // dq들어가는쪽 값이 연속으로 중복된 값이 아닐때 add
-            if(dq.peekFirst() != arr[i])
-                dq.addFirst(arr[i]);
+        Deque<Integer> dq = new ArrayDeque<>(); // dq 선언
+        dq.add(arr[0]);                         // dq에 첫번째 원소 넣고
+        for(int i=1;i<arr.length;i++){          // arr 순회하면서 
+            if(dq.peekFirst() != arr[i])        // arr값이 중복이 아닐때
+                dq.addFirst(arr[i]);            // push
         }
         
         while(!dq.isEmpty())
-            answer.add(dq.pollLast());     
+            answer.add(dq.pollLast());          // 뒤로 빼서 answer 저장
         
         return answer;
     }

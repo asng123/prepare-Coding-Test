c9584ecefcf3cfc8ee9d76f38422b949c09bf751
asng123 <80505960+asng123@users.noreply.github.com>
2023-01-06 10:44:21 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -5,29 +5,25 @@ class Solution {
     public ArrayList solution(int[] progresses, int[] speeds) {
         ArrayList<Integer> answer = new ArrayList<>();
       
-        int idx = 0;
-        int size = progresses.length;
-        while(idx < size){
-            //매일 speeds 만큼 진행시키기
-            for(int i=idx;i<size;i++){
-                progresses[i] += speeds[i];
-            }
+        int idx = 0;                    // 순회할 idx
+        int size = progresses.length;   // idx limit
+        while(idx < size){                              // idx가 size를 넘지않을때 까지
+            for(int i=idx;i<size;i++)                   // progresses를 순회하며
+                progresses[i] += speeds[i];             // speeds만큼 진행
             
             boolean chk = true;
             int cnt = 0;
-            // 연속적으로 진도율 100이상인것들 cnt하기
-            while(chk){
-                if(idx<size&&progresses[idx] >= 100){
-                    idx++;
-                    cnt++;
-                }else{
-                    chk = false;
+            while(chk){                                 // chk가 true일 동안
+                if(idx<size&&progresses[idx] >= 100){   // 범위내에서 진행율이 100% 이상인것을
+                    cnt++;                              // 세주고
+                    idx++;                              // idx를 증가시킨다.
+                }else{                                  // 진행률이 100%가 아니면 연속적으로 넣어줘야하므로
+                    chk = false;                        // 중단시키기위해 chk를 false로 바꾼다.
                 }
             }
-            // 한번에 배포되는거 answer에 넣기
-            if(cnt>=1){
-                answer.add(cnt);
-            }
+            
+            if(cnt>=1)              // cnt가 1 이상이면
+                answer.add(cnt);    // 한번에 배포되는 양 answer에 입력
         }
         
         return answer;

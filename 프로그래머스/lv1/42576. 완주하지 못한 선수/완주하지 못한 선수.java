2b5019bbfbe9b8fa29a087b6cdc5455898e5120b
asng123 <80505960+asng123@users.noreply.github.com>
2023-01-06 09:59:44 +0900

    Update 완주하지 못한 선수.java

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,32 +1,25 @@
 import java.util.*;
 import java.io.*;
 
+// HashMap
+// put, get, getOrDefault, remove(key), values(), keySet(), containskey, containsValue
+// size, clear, isEmpty
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        HashMap<String,Integer> map = new HashMap<>();
+        String answer = ""; // 답을 제출할 answer
         
-        //Hash 기본 입력 받는 방법
-        // for(int i = 0; i< participant.length; i++){
-        //     if(map.containsKey(participant[i]))
-        //        map.put(participant[i], map.get(participant[i])+1);
-        //     else
-        //         map.put(participant[i],1);
-        // }
-        
-        for(int i = 0; i< participant.length; i++){
-            map.put(participant[i], map.getOrDefault(participant[i],0)+1);  
+        HashMap<String,Integer> map = new HashMap<>();  // 마라톤 경기에 참여하는 인원     
+        for(int i = 0; i< participant.length; i++){     // 참여인원 순회하면서 넣기
+            map.put(participant[i], map.getOrDefault(participant[i],0)+1);  //★*100
         }
         
-        // 완주한 선수 제거, 동명이인 가능성 있어 -1 해준다.
-        for(int i=0;i<completion.length;i++){
-            map.put(completion[i],map.get(completion[i])-1);
+        for(int i=0;i<completion.length;i++){                   // 완주자 순회
+            map.put(completion[i],map.get(completion[i])-1);    // 완주자 인원 빼주기
         }
         
-        // 완주 못한선수를 answer로
-        for(int i=0;i<participant.length;i++){
-            if(map.get(participant[i]) !=0){
-                answer = participant[i];  
+        for(int i=0;i<participant.length;i++){  // map을 순회하면서
+            if(map.get(participant[i]) !=0){    // 남은 인원이 있다면
+                answer = participant[i];        // answer에 넣기
                 break;
             }
         }

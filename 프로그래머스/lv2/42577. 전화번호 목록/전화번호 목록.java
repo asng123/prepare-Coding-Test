c147c89cb70ded098afe4f702839e56678924c21
asng123 <80505960+asng123@users.noreply.github.com>
2023-01-06 10:22:08 +0900

    [level 2] Title: 전화번호 목록, Time: 362.47 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,22 +3,19 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        
-        int n = phone_book.length;
-        
-        // 정렬을 하면 똑같은 번호끼리 순차적으로 붙게 되어있다.
-        // 따라서 앞뒤로 확인만 하면 된다.
-        Arrays.sort(phone_book);
-        for(int i = 0; i<n-1; i++){
-            String s = phone_book[i];
-            int slen = s.length();
-            if(slen<=phone_book[i+1].length()){
-                if(s.equals(phone_book[i+1].substring(0,slen)))
-                    answer = false;
+        int len = phone_book.length;    // phone_book의 길이
+        Arrays.sort(phone_book);        // phone_book 정렬
+                                        // 문자열을 정렬하면 같은 숫자끼리 붙어있게 된다.
+                                        // 그점을 활용하며 비교
+        for(int i=0;i<len-1;i++){                               // phone_book을 순회하며   
+            String s = phone_book[i];                           // 각 문자열 길이를 체크해서
+            int slen = s.length();                                  
+            if(slen<=phone_book[i+1].length()){                 // 비교할 문자열과의 길이를 비교하고
+                if(s.equals(phone_book[i+1].substring(0,slen))) // 길이가 긴쪽을 substring하여 같다면 
+                    return false;                               // 접두어가 겹치므로 return false
             }
         }
         
-        return answer;
+        return true;
     }
 }

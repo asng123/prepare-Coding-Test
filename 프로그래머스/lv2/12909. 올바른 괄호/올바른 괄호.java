e503dab70f0b03dfeeee01e43d52823805cdb33c
asng123 <80505960+asng123@users.noreply.github.com>
2023-01-06 10:09:17 +0900

    Update 올바른 괄호.java

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,23 +3,21 @@ import java.io.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;  // 괄호가 올바른지 return할 답
-        
         Stack<Character> st = new Stack<>();    // 괄호가 올바른지 check할 stack
         for(int i = 0; i< s.length(); i++){     // s를 순회하면서
             if(s.charAt(i) == '('){             // '(' 열린괄호를 만나면 
                 st.add(s.charAt(i));            // stack에 add
             }else{                              // ')' 닫힌괄호를 만나면
-                if(st.size()>0){                // stack에 열린괄호가 들어있다면
+                if(st.size()>0)                 // stack에 열린괄호가 들어있다면
                     st.pop();                   // 짝이 맞으므로 pop하기
-                }else{                          // stack에 열린괄호가 없다면
-                    return false;             // 짝이 안맞아 올바르지 않으므로 false
-                }
+                else                            // stack에 열린괄호가 없다면
+                    return false;               // 짝이 안맞아 올바르지 않으므로 false
             }
         }
-        if(st.size()>0)
-            return false;
+        
+        if(st.size()>0)                         // 닫히는 괄호가 없는데 열린괄호가 남았다면
+            return false;                       // return false
 
-        return true;
+        return true;                            // 완벽하게 끝났다면 return true
     }
 }

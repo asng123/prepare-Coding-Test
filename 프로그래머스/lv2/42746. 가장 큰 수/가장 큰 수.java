707517f414c581d68647873bd804660953d2996b
asng123 <80505960+asng123@users.noreply.github.com>
2023-01-06 11:06:22 +0900

    [level 2] Title: 가장 큰 수, Time: 6.25 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -4,23 +4,21 @@ import java.io.*;
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
-        // numbers String 치환 배열
-        String[] nums = new String[numbers.length];
-        // nums에 자리수 String으로 변경후 대입
-        for (int i=0; i<nums.length; i++) 
-            nums[i] = numbers[i] + "";
-        // arr정렬방법(람다식)
-        Arrays.sort(nums, (o1, o2) -> {
-            return (o2+o1).compareTo(o1+o2); 
-        });
-        // 정답 넣을 ans, String에 +하는거 오래걸린다.
-        StringBuilder ans = new StringBuilder();
-        // 정답 넣기
-        for (int i=0; i<numbers.length; i++)
-            ans.append(nums[i]);
-        // 첫글자가 0이라면 return 0
-        if(ans.toString().charAt(0) == '0')
-            return "0";
-        return ans.toString();
+        String[] nums = new String[numbers.length];     // numbers String 치환 배열
+        for (int i=0; i<nums.length; i++)               // numbers를 순회하며
+            nums[i] = numbers[i] + "";                  // String으로 변환
+
+        Arrays.sort(nums, (o1, o2) -> {                 // arr정렬 방법(람다식)
+            return (o2+o1).compareTo(o1+o2);            // ★610이 큰지 106이 큰지 비교함★
+        });                                           
+        
+        StringBuilder ans = new StringBuilder();        // 정답 넣을 SB
+
+        for (int i=0; i<numbers.length; i++)            // nums를 순회하면서
+            ans.append(nums[i]);                        // ans에 넣어주기
+        if(ans.toString().charAt(0) == '0')             // 첫글자가 0이라면
+            return "0";                                 // return 0
+        
+        return ans.toString();                          // 그게아니라면 답 제출
     }
 }

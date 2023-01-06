0ad4404a69d1c78eee92ead87e4cab78840f7a58
asng123 <80505960+asng123@users.noreply.github.com>
2023-01-06 10:16:03 +0900

    Update ［3차］ 파일명 정렬.java

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -6,34 +6,29 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] files) {
-        Arrays.sort(files, (o1, o2) -> {
-            String head1 = o1.split("[0-9]")[0].toLowerCase(); // head - number, tail 분리
-            String head2 = o2.split("[0-9]")[0].toLowerCase(); // [0]은 잘린것 중에 첫번째 부분 즉 head
-            if (head1.compareTo(head2) == 0) { // head 같은문자열인 경우
-                String numTail1 = o1.substring(head1.length()); // number,tail 남김
-                String numTail2 = o2.substring(head2.length());
-                //Integer.parseInt("0023") = 23 출력된다. 앞에 0 잘림.
-                return Integer.parseInt(calNumber(numTail1)) - Integer.parseInt(calNumber(numTail2));
+        Arrays.sort(files, (o1, o2) -> {                        // files를 문제에서 주어진 조건에 맞춰 정렬한다
+            String head1 = o1.split("[0-9]")[0].toLowerCase();  // split("[0-9]"):문자열을 숫자를 기준으로 split,
+            String head2 = o2.split("[0-9]")[0].toLowerCase();  // return된 배열중 첫번째를 선택
+            if (head1.compareTo(head2) == 0) {                  // head가 같은문자열인 경우, number 기준으로 정렬
+                String numTail1 = o1.substring(head1.length()); // substring을 통해 head - number,tail 분리
+                String numTail2 = o2.substring(head2.length());               
+                return Integer.parseInt(calNumber(numTail1)) - Integer.parseInt(calNumber(numTail2)); // 앞에 0 잘림
             } else {
-                //head가 같은 문자열 아니면 그냥 compareTo 하면된다.
-                return head1.compareTo(head2);
+                return head1.compareTo(head2);                  // head가 다르다면 head기준 정렬
             }
         });
+        
         return files;
     }
-    
-    //number와 tail을 분리하는 함수
-    private static String calNumber(String numTail2) { //최대 5자리인 number 계산
-        StringBuilder sb2 = new StringBuilder();
-        for (char c : numTail2.toCharArray()) {
-            if (Character.isDigit(c) && sb2.length() <= 5) {
-                sb2.append(c);
-            } else {
-                // 숫자 -> 문자 형태인 문자열이기 때문에 숫자가 아닐때 바로 return 하면된다.
-                return sb2.toString();
-            }
+
+    private static String calNumber(String numTail) {           // number와 tail을 분리하는 함수
+        StringBuilder num = new StringBuilder();                // number를 담을 SB               
+        for (char c : numTail.toCharArray()) {                  // numTail을 char배열로 변환해 순회
+            if (Character.isDigit(c) && num.length() <= 5)      // c가 숫자라면
+                num.append(c);                                  // 넣기
+            else                                                // c가 문자라면
+                return num.toString();                          // num-tail 분리 끝
         }
-        // tail이 없는 경우도 있어 for문이 끝나면 sb2.toString()을 return한다.
-        return sb2.toString();
+        return num.toString();                                  // tail이 없는 경우 return해줄 곳
     }
 }

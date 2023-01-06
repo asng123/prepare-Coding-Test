97fac8fcf062450a37ed0cc89aa5231af273568e
asng123 <80505960+asng123@users.noreply.github.com>
2023-01-06 17:14:47 +0900

    Update 숫자의 표현.java

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -3,18 +3,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        
-        for(int i=1;i<=n;i++){
-            int temp = 0;
-            int plus = i;
-            while(temp<n){
-                temp += plus++;
-                if(temp == n)
-                    answer++;
+        int answer = 0;             // 개수를 세는 변수
+        for(int i=1;i<=n;i++){      // i: 더하기 시작지점
+            int temp = 0;           // 더해볼 변수
+            int plus = i;           // 시작지점 저장 변수
+            while(temp<n){          // n보다 작을때 까지
+                temp += plus++;     // 시작지점부터 쭉 더하면서
+                if(temp == n)       // n과 같아진다면
+                    answer++;       // 개수세기
             }
         }
         
-        return answer;
+        return answer;              // 답안 출력
     }
 }

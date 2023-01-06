0550fe439d25d27803b200ad3b897228260c0608
asng123 <80505960+asng123@users.noreply.github.com>
2023-01-06 10:07:17 +0900

    [level 2] Title: 올바른 괄호, Time: 17.41 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,24 +3,23 @@ import java.io.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        Stack<Character> st = new Stack<>();
+        boolean answer = true;  // 괄호가 올바른지 return할 답
         
-        for(int i = 0; i< s.length(); i++){
-            if(s.charAt(i) == '('){
-                st.add(s.charAt(i));
-            }else{
-                if(st.size()>0){
-                    st.pop();
-                }else{
-                    answer = false;
-                    break;
+        Stack<Character> st = new Stack<>();    // 괄호가 올바른지 check할 stack
+        for(int i = 0; i< s.length(); i++){     // s를 순회하면서
+            if(s.charAt(i) == '('){             // '(' 열린괄호를 만나면 
+                st.add(s.charAt(i));            // stack에 add
+            }else{                              // ')' 닫힌괄호를 만나면
+                if(st.size()>0){                // stack에 열린괄호가 들어있다면
+                    st.pop();                   // 짝이 맞으므로 pop하기
+                }else{                          // stack에 열린괄호가 없다면
+                    return false;             // 짝이 안맞아 올바르지 않으므로 false
                 }
             }
         }
         if(st.size()>0)
-            answer = false;
+            return false;
 
-        return answer;
+        return true;
     }
 }

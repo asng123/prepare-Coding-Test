b6915aaa6656b87e49ab5fc083b15b38036effbf
asng123 <80505960+asng123@users.noreply.github.com>
2023-10-13 23:16:44 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,14 @@
+# 동물보호소에 들어온 동물정보
+# 동물 보호소에서 입양보낸 동물 정보
+
+#입양 못간 동물중 가장 오래 보호소에 있었떤 동물 3마리의
+
+select i.name, i.datetime
+from animal_ins i
+left outer join animal_outs o
+on i.animal_id = o.animal_id
+where o.animal_id is null
+order by i.datetime
+limit 3
+
+

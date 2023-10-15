059a49c6f3ec7fe105babf00e70018490944d7fe
asng123 <80505960+asng123@users.noreply.github.com>
2023-10-15 09:32:25 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -5,10 +5,8 @@
 
 select i.name, i.datetime
 from animal_ins i
-left outer join animal_outs o
+left join animal_outs o
 on i.animal_id = o.animal_id
 where o.animal_id is null
 order by i.datetime
 limit 3
-
-
